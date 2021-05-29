848bf4cb5076e47a0fef06fd977dbcde688222f6
Artur Grudkowski <agrudkow@gmail.com>
2021-05-29 22:37:52 +0200

    refactor: add venv, format code

--- graph.py
+++ graph.py
@@ -1,33 +1,34 @@
 class Graph:
-    def __init__(self, links, demands):
-        self._links = self.load_links_to_map(links)
-        self._demands = demands
 
-    def is_path_correct(self, path):
-        for i in range(len(path) - 1):
-            if path[i+1] not in self._links[path[i]]:
-                return False
+  def __init__(self, links, demands):
+    self._links = self.load_links_to_map(links)
+    self._demands = demands
 
-        return True
+  def is_path_correct(self, path):
+    for i in range(len(path) - 1):
+      if path[i + 1] not in self._links[path[i]]:
+        return False
 
-    def load_links_to_map(self, links):
-        graph = dict()
-        for link in links:
-            if link[0].text not in graph:
-                graph[link[0].text] = set()
-            if link[1].text not in graph:
-                graph[link[1].text] = set()
+    return True
 
-            graph[link[0].text].add(link[1].text)
-            graph[link[1].text].add(link[0].text)
+  def load_links_to_map(self, links):
+    graph = dict()
+    for link in links:
+      if link[0].text not in graph:
+        graph[link[0].text] = set()
+      if link[1].text not in graph:
+        graph[link[1].text] = set()
 
-        print(graph)
-        return graph
+      graph[link[0].text].add(link[1].text)
+      graph[link[1].text].add(link[0].text)
 
-    def get_number_of_vertices(self):
-        return len(self._links)
+    print(graph)
+    return graph
 
-    def get_vertex_by_index(self, idx):
-        if idx < self.get_number_of_vertices():
-            return list(self._links.keys())[idx]
-        return None
+  def get_number_of_vertices(self):
+    return len(self._links)
+
+  def get_vertex_by_index(self, idx):
+    if idx < self.get_number_of_vertices():
+      return list(self._links.keys())[idx]
+    return None
